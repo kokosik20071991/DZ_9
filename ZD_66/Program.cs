b5f2bb55a1654b69f4ebd3b8dbb4a1b6c1af629d
Viktoria <vikatim4encko@yandex.ru>
2023-01-08 23:45:42 +0300

    Задачи 66

---
+++ ZD_66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+void NumSum(int M, int N, int sum)
+{
+    if (M > N)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (M++);
+    NumSum(M, N, sum);
+}
+
+NumSum(M, N, 0);
