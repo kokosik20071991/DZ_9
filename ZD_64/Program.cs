5975a19257fd52b157b2500bf1f428a2e0fc65b8
Viktoria <vikatim4encko@yandex.ru>
2023-01-08 23:39:58 +0300

    Задачи 64

---
+++ ZD_64/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите натуральное число больше 1: ");
+int n = int.Parse(Console.ReadLine());
+
+void NumCount(int n)
+{
+    if (n < 0) Console.Write($"{n} введено не натуральное число.");
+    if (n == 0) return;
+    Console.Write("{0, 4}", n);
+    NumCount(n - 1);
+}
+
+NumCount(n);
