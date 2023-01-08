52e9db63811ef401dca31f2277ffbe54f3cc40b5
Viktoria <vikatim4encko@yandex.ru>
2023-01-08 23:52:44 +0300

    Задачи 68

---
+++ ZD_68/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите положительное число M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write("Введите положительное число N: ");
+int N = int.Parse(Console.ReadLine());
+
+
+int AckermannFun(int M, int N)
+{
+    if (M == 0) return N + 1;
+    if (M != 0 && N == 0) return AckermannFun(M - 1, 1);
+    if (M > 0 && N > 0) return AckermannFun(M - 1, AckermannFun(M, N - 1));
+    return AckermannFun(M, N);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел ({M}, {N}) = {AckermannFun(M, N)}");
